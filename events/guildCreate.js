6be6a6dd7dcfa3f70d792c15c7582f46e7777ee3
MTGT01 <53175984+MTGT01@users.noreply.github.com>
2019-08-18 02:07:50 +0300

    Create guildCreate.js

---
+++ events/guildCreate.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+const girismesaj = [
+  '**MTGT Bot Başarı ile Sunucunuza Eklendi',
+  '**MTGT** sunucunuzdaki insanlara kolaylıklar sağlar.',
+  'Ve Onları Eğlendirir :)'
+]
+
+client.on('guildCreate', guild => {
+    const generalChannel = guild.defaultChannel
+    generalChannel.sendMessage(girismesaj)
+	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
+})
